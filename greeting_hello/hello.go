ed7e77966a8aba8782981c1b03ba831936a415c9
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-06-24 13:47:10 +0900

    finished error handle learn

--- greeting_hello/hello.go
+++ greeting_hello/hello.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	// ログエントリプレフィックスと時間、ソースファイル、行番号
+	// 印刷を無効にするフラグなど、事前定義されたロガーのプロパティを設定します。
+	log.SetPrefix("greetings:")
+	log.SetFlags(0)
+
 	// あいさつメッセージを取得して出力します。
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// エラーが返されない場合は、
+	// 返されたメッセージをコンソールに出力します
 	fmt.Println(message)
 }
--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Helloは、指定された人のあいさつを返します。
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// 名前が指定されていない場合は、メッセージと共にエラーを返します。
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// メッセージに名前を埋め込む挨拶を返します。
 	// message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	var message string = fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
